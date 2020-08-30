68fd981965bad591a9d8c51b282913a0b38e3b54
xtq——like <68905842+xiaotaiqi@users.noreply.github.com>
2020-08-30 13:54:14 +0800

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -5,8 +5,11 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.OUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT
+            ship.rect.centerx += 1
             
-def uplate_screen(ai_settings,screen,ship):
+def update_screen(ai_settings,screen,ship):
     scrren.fill(ai_settings.bg_color)
     ship.blitme()
     pygame.display.flip()
