e0388689160cce3b5a70a5319aea172c032c5eda
xtq——like <68905842+xiaotaiqi@users.noreply.github.com>
2020-08-30 13:34:42 +0800

    Create alien_invasion.py

---
+++ alien_invasion.py
@@ -0,0 +1,32 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+def run_game():
+    pygame.init()
+   ai_settings = Settings()
+   screen = pygame.display.set_mode(
+       (ai_settings.screen_width, ai_settings.screen_height)
+   )
+
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
+    bg_color = (230,230,230)
+
+    #开始游戏的主循环
+
+    while True:
+        #监视键盘和鼠标
+        for event in pygame.event.get():
+            if event.type == pygame.OUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+                #让屏幕可见
+
+        pygame.display.flip()
+
+run_game()
