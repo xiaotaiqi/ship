30ec46d0b2b2e4763bfe45022911264e93e99d45
xtq——like <68905842+xiaotaiqi@users.noreply.github.com>
2020-08-30 13:48:03 +0800

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -5,3 +5,8 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.OUIT:
             sys.exit()
+            
+def uplate_screen(ai_settings,screen,ship):
+    scrren.fill(ai_settings.bg_color)
+    ship.blitme()
+    pygame.display.flip()
